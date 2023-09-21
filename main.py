5fb93c242ae89cc5344e0acb4be5d34cc7cb9a12
Kassim <kassimbouzoubaa05@gmail.com>
2023-09-21 19:20:23 +0200

    Ajout de commentaires

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+
+# La fonction suivante permet de récupérer toutes les informations d'un livre
 def extract_book_info(book_url, name):
     product_page_url = "http://books.toscrape.com/catalogue/" + book_url
     url = product_page_url
@@ -26,6 +28,7 @@ def extract_book_info(book_url, name):
     image_url = "http://books.toscrape.com/" + soup.find('div', class_="item active").find('img').get('src')
     img_name = soup.find('div', class_="item active").find('img').get('alt')
     response = requests.get(image_url)
+    #Le code précédent permet d'extraire les données
 
     information_product = [
         {"product_page_url": product_page_url},
@@ -46,6 +49,7 @@ def extract_book_info(book_url, name):
     for info in information_product:
         header.append(list(info.keys())[0])
         description.append(list(info.values())[0])
+    # Le code précédent permet de transformer les données
 
     with open('data/donnee/' + name + '.csv', 'a') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
@@ -54,24 +58,28 @@ def extract_book_info(book_url, name):
 
     with open('data/images/' + img_name + '.jpg', 'wb') as image_file:
         image_file.write(response.content)
+    # Le code précédent permet de charger les données
 
 
+# La fonction suivante permet d'extraire toutes les informations de livres d'une catégorie
 def extract_category(cat_name, cat_url):
     url_category = "https://books.toscrape.com/" + cat_url
     page_category = requests.get(url_category)
     soup_category = BeautifulSoup(page_category.content, 'html.parser')
 
-    total_books = int(soup_category.find_all('strong')[1].text)
-    pages = total_books // 20 + (total_books % 20 > 0)
+    total_books = int(soup_category.find_all('strong')[1].text) # Récupère le nombre de livre et le transforme en int
+    pages = total_books // 20  # Division entière pour savoir le nombre de page
+    if total_books % 20 > 0 : # Rajoute une page si le reste est positif pour afficher les derniers livres
+        pages += 1
 
-    for page_number in range(1, pages + 1):
+    for page_number in range(1, pages + 1): # Itération sur chaque page
         if page_number > 1:
             url_category = url_category.replace('index.html', 'page-') + str(page_number) + ".html"
 
         book_list = soup_category.find_all('div', class_="image_container")
         book_urls = [book.find('a').get('href') for book in book_list]
 
-        for book_url in book_urls:
+        for book_url in book_urls: # Itération sur chaque livre de la page
             book_url = book_url.replace('../../../', '')
             extract_book_info(book_url, cat_name)
 
@@ -93,7 +101,7 @@ for link in all_links:
     category_links.append(category_url)
     category_names.append(category_name)
 
-for idx, category_name in enumerate(category_names):
+for idx, category_name in enumerate(category_names): # Itération sur chaque catégorie
     category_url = category_links[idx]
     extract_category(category_name, category_url)
 
